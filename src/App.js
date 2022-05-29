bc431a3d0b656a97d6ba6b34b51b2f34cf179c5c
Manikandan <nmanikandan2211@gmail.com>
2022-05-29 20:41:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+import { Quiz } from './screens';
+
+const App = () => {
+  return (
+    <Quiz />
+  );
+};
+
+export default App;
